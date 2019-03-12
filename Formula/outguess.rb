b4dcea56d99eaeff5afb7591b3d21ca943bb3543
Jaden Pleasants <jadenpleasants@fastmail.com>
2019-03-12 16:29:50 -0500

    added gzip as a build dependency for outguess

--- Formula/outguess.rb
+++ Formula/outguess.rb
@@ -4,6 +4,8 @@ class Outguess < Formula
     homepage "https://web.archive.org/web/20150419030527/http://www.outguess.org/"
     url "https://web.archive.org/web/20120118095110/http://www.outguess.org/outguess-0.2.tar.gz"
     sha256 "2f951ed7b9b9373fae8fe95616d49c83ae246cf53a2b60a82814228515bfa7d6"
+
+    depends_on "gzip" => :build
     
     def install
         system "./configure", "--prefix=#{prefix}", "--mandir=#{prefix}/share/man"
