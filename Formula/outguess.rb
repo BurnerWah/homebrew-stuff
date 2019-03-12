77222232a8f0cffcf3a1ecc243d863dde2d5f294
Jaden Pleasants <jadenpleasants@fastmail.com>
2019-03-12 15:11:50 -0500

    added outguess 0.2

---
+++ Formula/outguess.rb
@@ -0,0 +1,12 @@
+class Outguess < Formula
+    version "0.2"
+    desc "Stenography tool"
+    homepage "https://web.archive.org/web/20150419030527/http://www.outguess.org/"
+    url "https://web.archive.org/web/20120118095110/http://www.outguess.org/outguess-0.2.tar.gz"
+    sha256 "2f951ed7b9b9373fae8fe95616d49c83ae246cf53a2b60a82814228515bfa7d6"
+    
+    def install
+        system "./configure", "--prefix=#{prefix}"
+        system "make", "install"
+    end
+end
